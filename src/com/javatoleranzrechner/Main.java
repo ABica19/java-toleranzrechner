af85b12dc38004f41c2e73454b9399532eddc2d5
codelove <abdurrahmaanbica123@gmail.com>
2024-11-14 16:06:26 +0100

    Added a static method called showMainMenu() on the Main class.

--- src/com/javatoleranzrechner/Main.java
+++ src/com/javatoleranzrechner/Main.java
@@ -3,8 +3,15 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        JOptionPane.showMessageDialog(null, "Welcome to Java Toleranzrechner", "Herzlich Willkommen", JOptionPane.PLAIN_MESSAGE);
 
-        String userInput = JOptionPane.showInputDialog("Geben Sie etwas ein: \n");
+    }
+
+
+    public static void showMainMenu(){
+        JOptionPane.showMessageDialog(null, "Welcome to Java Toleranzrechner", "Herzlich Willkommen", JOptionPane.PLAIN_MESSAGE);
+        String userInput = JOptionPane.showInputDialog("To proceed, please select one of the following options:\n"+"1. Start Calculator"+"\n"+"2.Exit");
+        int userInputAsInt = Integer.parseInt(userInput);
+        
+
     }
 }
