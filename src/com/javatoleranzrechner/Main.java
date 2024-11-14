0ae85441f6c9b3f1978881cdcd76b15f827e89fd
codelove <abdurrahmaanbica123@gmail.com>
2024-11-14 16:07:42 +0100

    Added a switch case block on the showMainMenu method of the main class.

--- src/com/javatoleranzrechner/Main.java
+++ src/com/javatoleranzrechner/Main.java
@@ -11,7 +11,11 @@ public class Main {
         JOptionPane.showMessageDialog(null, "Welcome to Java Toleranzrechner", "Herzlich Willkommen", JOptionPane.PLAIN_MESSAGE);
         String userInput = JOptionPane.showInputDialog("To proceed, please select one of the following options:\n"+"1. Start Calculator"+"\n"+"2.Exit");
         int userInputAsInt = Integer.parseInt(userInput);
-        
+        switch(userInputAsInt){
+            case 1:
+                break;
+            case 2:
+        }
 
     }
 }
