594d75947e21c45f1d604b622c5d378b4973472c
codelove <abdurrahmaanbica123@gmail.com>
2024-12-17 22:44:10 +0100

    Eliminated the whole code on the main class in order to structure the project from scratch.

--- src/com/javatoleranzrechner/Main.java
+++ src/com/javatoleranzrechner/Main.java
@@ -3,19 +3,10 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
+        String userInput = JOptionPane.showInputDialog("1.Berechnung starten"+"\n"+"2.Programm beenden");
+        int chosenNumber = Integer.parseInt(userInput);
+        JOptionPane.showMessageDialog(null,"Chosen Number was: "+chosenNumber);
 
-    }
-
-
-    public static void showMainMenu(){
-        JOptionPane.showMessageDialog(null, "Welcome to Java Toleranzrechner", "Herzlich Willkommen", JOptionPane.PLAIN_MESSAGE);
-        String userInput = JOptionPane.showInputDialog("To proceed, please select one of the following options:\n"+"1. Start Calculator"+"\n"+"2.Exit");
-        int userInputAsInt = Integer.parseInt(userInput);
-        switch(userInputAsInt){
-            case 1:
-                break;
-            case 2:
-        }
 
     }
 }
